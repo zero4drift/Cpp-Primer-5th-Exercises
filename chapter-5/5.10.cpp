133f17c25ac28d8024be52416454ea86586e6cb5
zero4drift <fang0052@e.ntu.edu.sg>
2018-04-21 22:01:35 +0800

    Correct the fault due to misunderstanding of a question.

--- chapter-5/5.10.cpp
+++ chapter-5/5.10.cpp
@@ -6,16 +6,38 @@ using std::endl;
 
 int main()
 {
-  unsigned vowelCnt = 0;
+  unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
   char ch;
-  while(cin >> ch)
-    {
-      switch(ch)
-	{
-	case 'a': case 'e': case 'i': case 'o': case 'u': case 'A': case 'E': case 'I': case 'O': case 'U':
-	  ++vowelCnt;
-	  break;
-	}
-    }
-  cout << vowelCnt << endl;
+  while (cin >> ch)
+    switch (ch)
+      {
+      case 'a':
+      case 'A':
+	++aCnt;
+	break;
+      case 'e':
+      case 'E':
+	++eCnt;
+	break;
+      case 'i':
+      case 'I':
+	++iCnt;
+	break;
+      case 'o':
+      case 'O':
+	++oCnt;
+	break;
+      case 'u':
+      case 'U':
+	++uCnt;
+	break;
+      }
+           
+  cout << "Number of vowel a(A): \t" << aCnt << '\n'
+       << "Number of vowel e(E): \t" << eCnt << '\n'
+       << "Number of vowel i(I): \t" << iCnt << '\n'
+       << "Number of vowel o(O): \t" << oCnt << '\n'
+       << "Number of vowel u(U): \t" << uCnt << endl;
+    
+  return 0;
 }
