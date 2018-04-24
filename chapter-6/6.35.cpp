9811e6dbc5fd5bc7ce5bdfb969326e37455a5cd7
zero4drift <fang0052@e.ntu.edu.sg>
2018-04-24 14:43:13 +0800

    Make a better statement in exercise 6.35.

--- chapter-6/6.35.cpp
+++ chapter-6/6.35.cpp
@@ -1,3 +1,3 @@
 // val-- keeps a copy of the original value of val, decreases val by 1, and then return the kept copy.
 
-// this function processing would never meet the end condition statement, consumes all stack trace.
+// this function processing would never meet the end condition statement, exceeds the maximum stack depth.
