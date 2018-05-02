ab0b78374e12fc196afc9f8ccd13d4d6eca7d2bc
zero4drift <fang0052@e.ntu.edu.sg>
2018-05-02 23:03:28 +0800

    After days spent in looking for a rent place, redo this chatper.

--- chapter-7/Sales_data.h
+++ chapter-7/Sales_data.h
@@ -1,56 +1,17 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 
-#include <string>
 #include <iostream>
 
-using std::cout;
-using std::endl;
-using std::string;
 using std::istream;
 using std::ostream;
 
-istream &read();
+struct Sales_data;
 
-struct Sales_data {
-  Sales_data() = default;
-Sales_data(const string &s): bookNo(s) {}
-Sales_data(const string &s, unsigned n, double p):
-  bookNo(s), units_sold(n), revenue(p*n) {}
-  Sales_data(istream &is) {read(is, *this)}
-  string isbn() const {return bookNo;}
-  Sales_data& combine(const Sales_data&);
-  double avg_price() const;
-  string bookNo;
-  unsigned units_sold = 0;
-  double revenue = 0.0;
-};
+istream &read(istream &is, Sales_data &item);
 
-Sales_data& Sales_data::combine(const Sales_data &rhs)
-{
-  units_sold += rhs.units_sold;
-  revenue += rhs.revenue;
-  return *this;
-}
+ostream &print(ostream &os, const Sales_data &item);
 
-istream &read(istream &is, Sales_data &item)
-{
-  double price;
-  is >> item.bookNo >> item.units_sold >> price;
-  item.revenue = price * item.units_sold;
-  return is;
-}
-
-ostream &print(ostream &os, const Sales_data &item)
-{
-  os << item.isbn() << " " << item.units_sold << " " << item.revenue << item.avg_price();
-  return os;
-}
-
-Sales_data add(const Sales_data &item1, const Sales_data &item2)
-{
-  Sales_data result = item1;
-  return result.combine(item2);
-}
+Sales_data add(const Sales_data &item1, const Sales_data &item2);
 
 #endif
