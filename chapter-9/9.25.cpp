2c816b791e79667ec27e28886766b2bf425c8b4d
zero4drift <fang0052@e.ntu.edu.sg>
2018-06-04 20:21:34 +0800

    Repair a typo.

--- chapter-9/9.25.cpp
+++ chapter-9/9.25.cpp
@@ -1,5 +1,5 @@
 // c.erase(elem1, elem2)
-// if elem1 and elem2 ponits to same element, the iterator range is empty, so erase nothing, return this elem1(or elem1, no matter, identical)
+// if elem1 and elem2 ponits to same element, the iterator range is empty, so erase nothing, return this elem1(or elem2, no matter, identical)
 // if elem2 is a end off iterator, remove the one pointed by elem1 and till the last one, return elem2
 // if elem1 and elem2 both end off iterator, remove nothing, return end off iterator
 
