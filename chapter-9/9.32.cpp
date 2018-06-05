cfa90f2e77eb0165e5cc9cd5ed66192ed381aa78
zero4drift <fang0052@e.ntu.edu.sg>
2018-06-05 15:37:42 +0800

    Make it more deatailed.

--- chapter-9/9.32.cpp
+++ chapter-9/9.32.cpp
@@ -1,5 +1,5 @@
 // iter = vi.insert(iter, *iter++);
 // illegal
 
-// for that the evaluation of parameters of a function calling maybe not in order,
+// for that the evaluation of arguments(initialization of parameters) of a function calling maybe not in order,
 // in this case, iter may increase it self at first or later, that calling may have different results per processing, which is not allowed.
