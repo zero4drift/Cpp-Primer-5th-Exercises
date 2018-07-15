f3b6b7038af7bbbb7f6edbd8da60f6af81e2c8af
zero4drift <fang0052@e.ntu.edu.sg>
2018-07-15 22:44:09 +0800

    Add a test sample for class My_unique_ptr.

--- chapter-16/16.28_my_unique_ptr.cpp
+++ chapter-16/16.28_my_unique_ptr.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
 #include "My_unique_ptr.h"
 #include "DebugDelete.h"
+
+using std::cout;
+using std::endl;
+
+void my_delete(int *p)
+{
+  cout << "function my_delete" << endl;
+  delete p;
+}
 
 int main()
 {
   My_unique_ptr<int> m1;
-  My_unique_ptr<int, DebugDelete> m2(new int(),DebugDelete());
+  My_unique_ptr<int, DebugDelete> m2(new int(), DebugDelete());
+  My_unique_ptr<int, decltype(my_delete)*> m3(new int(), my_delete);
 }
