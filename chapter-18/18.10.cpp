7e78b67a3e7148db2f7253230110d415c7a48cfb
zero4drift <fang0052@e.ntu.edu.sg>
2018-08-01 00:16:00 +0800

    Two more...

--- chapter-18/18.10.cpp
+++ chapter-18/18.10.cpp
@@ -6,27 +6,30 @@ using std::cerr;
 using std::endl;
 using std::cin;
 
-// int main()
-// {
-//   Sales_data item1, item2, sum;
-//   while(cin >> item1 >> item2);
-//   sum = item1 + item2;
-// }
-
-
 int main()
 {
   Sales_data item1, item2, sum;
   while(cin >> item1 >> item2)
-    {
-      try
-	{
-	  sum = item1 + item2;
-	}
-      catch(const isbn_mismatch &e)
-	{
-	  cerr << e.what() << ": left isbn(" << e.left
-	       << ") right isbn(" << e.right << ")" << endl;
-	}
-    }
+    sum = item1 + item2;
+  // just like said in section 18.1.1 
+  // for on throwed exception, if there is no matched catch expression
+  // program calls std library function terminate to terminate program processing
 }
+
+
+// int main()
+// {
+//   Sales_data item1, item2, sum;
+//   while(cin >> item1 >> item2)
+//     {
+//       try
+// 	{
+// 	  sum = item1 + item2;
+// 	}
+//       catch(const isbn_mismatch &e)
+// 	{
+// 	  cerr << e.what() << ": left isbn(" << e.left
+// 	       << ") right isbn(" << e.right << ")" << endl;
+// 	}
+//     }
+// }
--- chapter-18/18.11.cpp
+++ chapter-18/18.11.cpp
@@ -1,1 +1,2 @@
-// Virtual function what of class type exception provides information related to 
+// Virtual function what of class type exception provides information related to throwed exception;
+// this function should help us handle the exception but not introduces a new exception, that should be guaranteed when programmer calls this function.
