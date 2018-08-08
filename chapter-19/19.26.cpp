05f2886e142c2088c187bdd8b925f925cd8f92ef
zero4drift <fang0052@e.ntu.edu.sg>
2018-08-08 15:48:56 +0800

    It is just around the corner!

---
+++ chapter-19/19.26.cpp
@@ -0,0 +1,5 @@
+extern "C" int compute(int *, int); // declares a C function which accpets two parateters of int * and int type, then its return value is int type;
+
+extern "C" double compute(double *, double); // declares a C function which accpets two parateters of double * and double type, then its return value is double type;
+
+// illegal, due to C programming language does not support overload functions.
